7afb59b7e4e9c77500c11ffb99ba2b103f4c1fd6
Your Name <mohsen86330@gmail.com>
2025-07-12 17:26:52 +0300

    Add new registration page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def login_page():
     return render_template('login.html')
 
+@app.route('/register')
+def register_page():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
